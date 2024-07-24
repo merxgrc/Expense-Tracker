a31292c88b8050668f893c44cca076e3b8c6618c
merxgrc <yeetgawdmcneckass@gmail.com>
2024-07-24 05:04:46 -0700

    added image to homepage

--- expense-tracker/src/Homepage.js
+++ expense-tracker/src/Homepage.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
+const comingsoon = {
+    imageUrl: 'https://media1.tenor.com/m/Y3Q-JG_JE1YAAAAC/cat-driving-serious.gif',
+    imageSize: 250
+}
+
 function Homepage() {
   const [user, setUser] = useState({});
 
@@ -13,6 +18,16 @@ function Homepage() {
   return (
     <div>
       <h2>Hello {user.name}</h2>
+      <h3>This page will be coming soon!</h3>
+      <img src={comingsoon.imageUrl}></img>
+      <div>
+        <div>
+            <h3>Income:</h3>
+        </div>
+        <div>
+            <h3>Expenses:</h3>
+        </div>
+      </div>
     </div>
   );
 }
